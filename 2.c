53baa34310a91cee4257d76e96c428fa2d48e47c
R170282 <107169425+R170282@users.noreply.github.com>
2022-08-01 22:41:41 +0530

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("int: %ld \n",sizeof(int));# format specilaizer consisit each and every data type
+    printf("float: %ld \n",sizeof(float));
+    printf("double: %ld \n",sizeof(double));
+    printf("char: %ld \n",sizeof(char));
+    return 0;
+}
+# c language vaild data tpes in c are int ,float,char,double
+
